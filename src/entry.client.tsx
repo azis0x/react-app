2b0220d1937a1ebcbb666f9e8f0bf5a5ee6f0708
azis0x <azis190407@gmail.com>
2026-01-29 02:43:13 +0700

    Rewrite entry client hydrate for support more lazy routes

--- src/entry.client.tsx
+++ src/entry.client.tsx
@@ -1,16 +1,37 @@
 import { StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 import "./style.css";
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, RouterProvider, matchRoutes } from "react-router";
 import { routes } from "./app/routes";
-import { RouterProvider } from "react-router/dom";
 
-let rootEl = document.getElementById("root") as HTMLDivElement;
-let router = createBrowserRouter(routes);
+hydrate();
 
-hydrateRoot(
-  rootEl,
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-);
+async function hydrate() {
+  // Determine if any of the initial routes are lazy
+  let lazyMatches = matchRoutes(routes, window.location)?.filter(
+    // @ts-ignore
+    (m) => m.route.lazy,
+  );
+
+  // Load the lazy matches and update the routes before creating your router
+  // so we can hydrate the SSR-rendered content synchronously
+  if (lazyMatches && lazyMatches?.length > 0) {
+    await Promise.all(
+      lazyMatches.map(async (m) => {
+        // @ts-ignore
+        let routeModule = await m.route.lazy!();
+        Object.assign(m.route, { ...routeModule, lazy: undefined });
+      }),
+    );
+  }
+
+  let rootEl = document.getElementById("root") as HTMLDivElement;
+  let router = createBrowserRouter(routes);
+
+  hydrateRoot(
+    rootEl,
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  );
+}
